39f78b189d367d055d218c7c90001996508cd254
Itai Bar-Haim <itai.barhaim@applitools.com>
2020-01-12 11:06:12 +0200

    Initial Commit.

---
+++ ApplitoolsTutorial/BasicDemo.cs
@@ -0,0 +1,63 @@
+﻿using Applitools.Appium;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+
+namespace ApplitoolsTutorial
+{
+    [TestFixture]
+    public class BasicDemo
+    {
+        private RemoteWebDriver driver;
+        private Eyes eyes;
+
+        [Test]
+        public void AndroidTest()
+        {
+            // Initialize the eyes SDK (IMPORTANT: make sure your API key is set in the APPLITOOLS_API_KEY env variable).
+            eyes = new Eyes();
+
+            // Set the desired capabilities.
+            AppiumOptions options = new AppiumOptions();
+            options.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Samsung Galaxy S10");
+            options.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "9.0");
+            options.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+            options.AddAdditionalCapability(MobileCapabilityType.BrowserName, "Chrome");
+            options.AddAdditionalCapability("deviceOrientation", "portrait");
+
+            // Initialize BrowserStack credentials. (IMPORTANT: make sure you have the below environment variables set).
+            Dictionary<string, object> browserstackOptions = new Dictionary<string, object>();
+            browserstackOptions.Add("userName",  Environment.GetEnvironmentVariable("BROWSERSTACK_USERNAME"));
+            browserstackOptions.Add("accessKey", Environment.GetEnvironmentVariable("BROWSERSTACK_ACCESS_KEY"));
+
+            options.AddAdditionalCapability("bstack:options", browserstackOptions);
+
+            driver = new RemoteWebDriver(new Uri("http://hub-cloud.browserstack.com/wd/hub/"), options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            driver.Url = $"https://applitools.com/helloworld";
+
+            // Start visual UI testing.
+            eyes.Open(driver, "Hello, Applitools!", "My first Appium Web C# test!");
+
+            // Visual UI testing.
+            eyes.CheckWindow("Hello, World!");
+
+            // End the test.
+            eyes.Close();
+        }
+
+        [TearDown]
+        public void AfterEach()
+        {
+            // Close the browser if driver isn't null.
+            driver?.Quit();
+
+            // If the test was aborted before eyes.close was called, ends the test as aborted.
+            eyes.AbortIfNotClosed();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Pre-requisites:
+
+1. Visual Studio installed on your machine.
+   * [Install it from here](https://visualstudio.microsoft.com/downloads/)
+2. BrowserStack account
+   * [Create a free account here](https://www.browserstack.com/)
+
+# Steps to run this example
+
+1. Git clone this repo
+    * `git clone https://github.com/applitools/tutorial-appium-web-csharp-basic.git`
+2. Open the folder `tutorial-appium-web-csharp-basic`
+3. Set your API key in the _APPLITOOLS_API_KEY_ environment variable.
+    * You can get your API key by logging into Applitools > Person Icon > My API Key.
+4. Set your BrowserStack user name and access key in the _BROWSERSTACK_USERNAME_ and _BROWSERSTACK_ACCESS_KEY_ environment variables respectively.
+    * You can get BrowserStack credentials by logging into BrowserStack > Automate > Access Key.
+5. Double click the `ApplitoolsTutorial.sln`. This will open the project in Visual Studio.
+
+   **MAKE SURE YOU HAVE THE ABOVE ENVIRONMENT VARIABLES SET BEFORE YOU START VISUAL STUDIO**
+6. Open the *Test Explorer* and run the test.
